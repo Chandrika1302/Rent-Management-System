e07976fa1a3699eb0ca399d5e6c3eabb8c653252
Chandrika1302 <gehini1302@gmail.com>
2023-02-18 21:11:48 +0530

    enable persisted login state

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 
-//TODO decide if persisted state is required
-// const persistedState = JSON.parse(localStorage.getItem("reduxState") ?? "{}");
+const persistedState = JSON.parse(localStorage.getItem("reduxState") ?? "{}");
 
 const store = configureStore({
   reducer: {
     user: userReducer,
   },
-  //   preloadedState: persistedState,
+  preloadedState: persistedState,
 });
 
-// store.subscribe(() => {
-//   localStorage.setItem("reduxState", JSON.stringify(store.getState()));
-// });
+store.subscribe(() => {
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+});
 
 export default store;
